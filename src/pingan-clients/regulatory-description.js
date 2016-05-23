0ae6277f4770c122e5d0d2df5d41c3dac5ef3469
Yi Yang <git@iyyang.com>
2016-05-23 21:45:25 +0800

    Updated file for array handling.

--- src/pingan-clients/regulatory-description.js
+++ src/pingan-clients/regulatory-description.js
@@ -1,14 +1,40 @@
+let C = String;
+let N = Number;
+
 let request = {
   6000: {
     keys: [
-      {key: 'FuncFlag', required: true, length: 1, default: '1'},
-      {key: 'SupAcctId', required: true, length: 32},
-      {key: 'ThirdCustId', required: true, length: 32},
-      {key: 'CustProperty', required: true, length: 2, default: '00'},
-      {key: 'NickName', required: false, length: 120},
-      {key: 'MobilePhone', required: false, length: 12},
-      {key: 'Email', required: false, length: 120},
-      {key: 'Reserve', required: false, length: 120}
+      {key: 'FuncFlag', required: true, type: C, length: 1, default: '1'},
+      {key: 'SupAcctId', required: true, type: C, length: 32},
+      {key: 'ThirdCustId', required: true, type: C, length: 32},
+      {key: 'CustProperty', required: true, type: C, length: 2, default: '00'},
+      {key: 'NickName', required: false, type: C, length: 120},
+      {key: 'MobilePhone', required: false, type: C, length: 12},
+      {key: 'Email', required: false, type: C, length: 120},
+      {key: 'Reserve', required: false, type: C, length: 120}
+    ]
+  },
+  6052: {
+    keys: [
+      {key: 'FuncFlag', required: true, type: C, length: 1, default: '1'},
+      {key: 'FuncFlag', required: true, type: C, length: 1},
+      {key: 'OutCustAcctId', required: true, type: C, length: 32},
+      {key: 'OutThirdCustId', required: true, type: C, length: 32},
+      {key: 'SupAcctId', required: true, type: C, length: 32},
+      {key: 'ThirdHtCount', required: true, type: C, length: 30},
+      {key: 'InCustAcctId', required: true, type: C, length: 32},
+      [
+        {key: 'InThirdCustId', required: true, type: C, length: 32},
+        {key: 'TranAmount', required: true, type: N, length: 15},
+        {key: 'TranFee', required: true, type: N, length: 15},
+        {key: 'CcyCode', required: true, type: C, length: 3, default: 'RMB'},
+        {key: 'ThirdHtId', required: true, type: C, length: 30},
+        {key: 'ThirdHtMsg', required: false, type: C, length: 500},
+        {key: 'Note', required: false, type: C, length: 120},
+        {key: 'MarketLogNo', required: true, type: C, length: 20}
+      ],
+      {key: 'Reserve', required: false, type: C, length: 120},
+      {key: 'WebSign', required: false, type: C, length: 256}
     ]
   }
 };
